1a36aca5f9231646a2f737fc4374876afd0c12e1
abhishek <abhisek@auzmor.com>
2021-07-28 23:42:46 +0530

    projectmilestonetoll first commit

---
+++ src/main/java/com/projectmilestonetool/ProjectMilestoneToolApplication.java
@@ -0,0 +1,13 @@
+package com.projectmilestonetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMilestoneToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMilestoneToolApplication.class, args);
+	}
+
+}
